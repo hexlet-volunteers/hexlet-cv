668fb54ce313a2920dde23d70c988e9d4cf1b555
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-03-07 09:56:07 +0300

    chore: установлены и настроены Husky, Prettier, обновлен конфиг ESLint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,6 +7,7 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 import fsdPlugin from 'eslint-plugin-fsd-lint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   {
@@ -16,56 +17,38 @@ export default tseslint.config([
       '.cache/',
       '*.config.js',
       '**/*.d.ts',
-      '**/toDelete/'
+      '**/toDelete/',
     ],
   },
-  stylistic.configs.recommended,
   globalIgnores(['dist']),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  fsdPlugin.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-      fsdPlugin.configs.recommended,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
     },
     ignores: ['**/*.test.ts'],
-    plugins: { react, fsd: fsdPlugin },
+    plugins: {
+      react,
+      fsd: fsdPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
       'fsd/forbidden-imports': 'error',
       'fsd/no-relative-imports': 'error',
       'fsd/no-public-api-sidestep': 'error',
-      'indent': ['error', 2,
-        {
-          SwitchCase: 1,
-          VariableDeclarator: 1,
-          MemberExpression: 1,
-          FunctionDeclaration: { body: 1, parameters: 2 },
-          FunctionExpression: { body: 1, parameters: 2 },
-          CallExpression: { arguments: 1 },
-        }],
-      'array-element-newline': ['error', 'consistent'],
-      'object-property-newline': ['error', { allowAllPropertiesOnSameLine: false }],
-      'block-spacing': ['error', 'always'],
-      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-      'comma-spacing': ['error', { before: false, after: true }],
-      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-      'space-before-blocks': ['error', 'always'],
-      'space-infix-ops': ['error', { int32Hint: false }],
-      'no-unused-vars': 'warn',
-      '@stylistic/comma-dangle': [
-        'error',
-        {
-          arrays: 'always-multiline',
-          objects: 'always-multiline',
-          functions: 'never',
-        },
-      ],
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
+  eslintConfigPrettier,
 ])
