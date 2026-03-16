7e120e20e755e665eb056934e07e296c7b0725ed
nathalieMalsh <nmalysheva99@mail.ru>
2026-03-16 14:31:20 +0300

    [feat-1026]: notifications сделаны опциональными в NotificationsListProps

--- frontend/src/widgets/notifications/ui/NotificationsList.tsx
+++ frontend/src/widgets/notifications/ui/NotificationsList.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next'
 import type { NotificationDTO } from '../types'
 
 interface NotificationsListProps {
-    notifications: NotificationDTO[]
+    notifications?: NotificationDTO[]
 }
 
 export const NotificationsList: React.FC<NotificationsListProps> = (props): JSX.Element => {
