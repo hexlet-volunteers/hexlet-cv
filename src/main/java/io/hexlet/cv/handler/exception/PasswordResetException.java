d8cecab255f76103b9cfc0af938ca78347f14a61
areldin8 <belphegor26ra@gmail.com>
2026-03-26 01:18:24 +0300

    chore: remove obsolete exception handlers, mappers and services

--- src/main/java/io/hexlet/cv/handler/exception/PasswordResetException.java
+++
@@ -1,8 +0,0 @@
-package io.hexlet.cv.handler.exception;
-
-public class PasswordResetException extends RuntimeException {
-
-    public PasswordResetException(String message) {
-        super(message);
-    }
-}
