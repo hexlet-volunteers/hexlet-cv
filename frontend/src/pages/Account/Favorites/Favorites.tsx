69ad2da2e6de4cfe16306a071dad6eefea28a617
GalinaBagram1987 <gaby_ev@mail.ru>
2026-07-03 22:22:16 +1000

    added skeleton and toDo comment

--- frontend/src/pages/Account/Favorites/Favorites.tsx
+++ frontend/src/pages/Account/Favorites/Favorites.tsx
@@ -1,6 +1,14 @@
 import { Link } from '@inertiajs/react'
 import { useState } from 'react'
-import { Text, Button, Pagination, Paper, Table, Box } from '@mantine/core'
+import {
+  Text,
+  Button,
+  Pagination,
+  Paper,
+  Table,
+  Box,
+  Skeleton,
+} from '@mantine/core'
 import { useTranslation } from 'react-i18next'
 import type { FavoriteDTO } from '@entities/favorite'
 
@@ -10,9 +18,10 @@ import type { FavoriteDTO } from '@entities/favorite'
 
 export interface FavoritesProps {
   list?: FavoriteDTO[]
+  isLoading?: boolean
 }
 
-const Favorites = ({ list }: FavoritesProps) => {
+const Favorites = ({ list, isLoading = false }: FavoritesProps) => {
   const { t } = useTranslation()
   const [activePage, setPage] = useState(1)
   const itemsPerPage: number = 6
@@ -23,6 +32,57 @@ const Favorites = ({ list }: FavoritesProps) => {
   const totalPages = Math.ceil((list?.length || 0) / itemsPerPage)
   const showPagination = totalPages > 1 // если страниц больше 1
 
+  //TODO: сделать серверную пагинацию
+
+  if (isLoading) {
+    return (
+      <Paper shadow="xs" radius="lg" withBorder p="md">
+        <Table withColumnBorders>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th style={{ width: '15%' }}>
+                <Skeleton height={24} width="60%" />
+              </Table.Th>
+              <Table.Th style={{ width: '70%' }}>
+                <Skeleton height={24} width="80%" />
+              </Table.Th>
+              <Table.Th
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Skeleton height={24} width="80%" />
+              </Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {Array.from({ length: 6 }).map((_, idx) => (
+              <Table.Tr key={idx}>
+                <Table.Td>
+                  <Skeleton height={20} width="50%" />
+                </Table.Td>
+                <Table.Td>
+                  <Skeleton height={20} width="80%" />
+                </Table.Td>
+                <Table.Td
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Skeleton height={36} width={100} radius="xl" />
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Paper>
+    )
+  }
+
   if (!list || list?.length === 0) {
     return (
       <Paper shadow="xs" radius="lg" withBorder p="md">
@@ -30,6 +90,7 @@ const Favorites = ({ list }: FavoritesProps) => {
       </Paper>
     )
   }
+
   const rows = paginatedList?.map((item) => (
     <Table.Tr key={item.id}>
       <Table.Td>
